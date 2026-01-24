13b7e912b657a69daa493ad0fff1373c962dc4bd
Leeq <leeqsocials@gmail.com>
2026-01-24 10:56:13 +0300

    Initial Commit

---
+++ src/components/HeroModels/HeroExp.jsx
@@ -0,0 +1,41 @@
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { useMediaQuery } from 'react-responsive';
+import { Room } from './Room';
+import { Leeq } from './Leeq';
+import HeroLights from './HeroLights';
+
+const HeroExp = () => {
+  const isTablet = useMediaQuery({ query: '(max-width: 1024px)' });
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
+
+  return (
+    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+        
+        
+        <OrbitControls 
+          enablePan={false}
+          enableZoom={!isTablet}
+          maxDistance={20}
+          minDistance={5}
+          minPolarAngle={Math.PI / 5}
+          maxPolarAngle={Math.PI / 2}
+        />
+
+        <HeroLights />
+
+        <group 
+          scale={isMobile ? 1.5 : 2.5}
+          position={isMobile ? [0, -2.5, 0] : [0, -1.5, 0]}
+          rotation={[Math.PI / 20, Math.PI / -4, 0]}
+        >
+        <Leeq />
+
+        </group>
+
+    </Canvas>
+  )
+}
+
+export default HeroExp
---
+++ src/components/sections/Hero.jsx
@@ -0,0 +1,83 @@
+import { words } from "../../Constants/index";
+import Button from "../Button";
+import HeroExp from "../HeroModels/HeroExp";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo('.hero-text h1',
+      {y: 50,
+        opacity: 0
+      },
+      {
+        y:0,
+        opacity:1,
+        stagger:0.5,
+        duration:1.5 ,
+        ease: 'power2.inOut',
+      },
+    )
+  })
+
+  return (
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="background" />
+      </div>
+
+      <div className="hero-layout">
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Results</h1>
+            </div>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Hi, I'm Leeq an upcoming SWE Student from Mekelle University
+              with a passion for creating cool UI designs and Games.
+            </p>
+
+            <Button 
+            className="md:w-80 md:h-16 w-70 h-15"
+            id="button"
+            text="Check My Work"
+            />
+          </div>
+        </header>
+
+        {/*3D model*/} 
+
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExp />
+          </div>
+        </figure>
+
+
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
