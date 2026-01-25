854884f53d8f521afcc82d8ba08904509e4b94ff
Leeq <leeqsocials@gmail.com>
2026-01-25 14:47:29 +0300

    Build Navbar,Showcase and add my logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import NavBar from './components/NavBar';
 import Hero from './components/sections/Hero'
+import ShowcaseSection from './components/sections/ShowcaseSection';
 
 const App = () => {
   return (
     <>
+      <NavBar/>
       <Hero />
+      <ShowcaseSection/>
     </>
   );
 };
