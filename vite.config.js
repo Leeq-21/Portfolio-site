f0a72874631377ceceb610a8e8708baad4b1d008
leeq <leeqsocials@gmail.com>
2026-01-27 22:12:50 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react(), tailwindcss()],
-  base: command === 'build' ? '/Portfolio-site' : '/',
-}))
+  base: process.env.VITE_BASE_PATH || '/Portfolio-site',
+})
